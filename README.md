ba78e8eea6921c1c3b5462fb9f9e77658fe9840d
domingosvanjo <domingosvanjo@gmail.com>
2025-01-29 11:02:20 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,10 @@
+## Jogo do amigo secreto
+
+<img src="assets/tela.png" width="60%" hight="20%" title="Jogo do amigo secreto" alt="figura mostrando como é a tela do jogo do amigo secreto"/> <br>
+Esta é uma aplicação onde você pode adicionar o nome de seus amigos numa lista e, depois, sortear seu amigo secreto. Ideal para brincadeiras de fim de ano.
+
+Divirta-se.
+
+## Mais sobre o projeto
+
+[![Repo Card](https://github-readme-stats.vercel.app/api/pin/?username=domingosvanjo&repo=jogo-amigo-secreto&bg_color=000&border_color=30A3DC&show_icons=true&icon_color=30A3DC&title_color=E94D5F&text_color=FFF)](https://github.com/domingosvanjo/jogo-amigo-secreto)
--- app.js
+++ app.js
@@ -1,1 +1,53 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigo;
+let amigos=[];
+let ulListAmigos = document.getElementById("listaAmigos");
+let resultado = document.getElementById("resultado");
+let botSortear = document.querySelector(".button-draw");
+let sorteado;
+
+botSortear.setAttribute('disabled', true);
+
+function adicionarAmigo(){
+    amigo = document.getElementById("amigo");
+    if (amigo.value != ""){
+        if(verifLista(amigo.value)){return;}
+        amigos.push(amigo.value.toUpperCase());
+        let li = document.createElement('li');
+        li.textContent = amigo.value;
+        ulListAmigos.appendChild(li);
+        resultado.innerHTML = "";
+        
+        if (amigos.length >= 2){
+            botSortear.removeAttribute('disabled');
+        }
+    }else{
+        alert('Digite um nome para inserir na lista!');
+    }
+    amigo.value = "";
+    amigo.focus();
+}
+
+function sortearAmigo(){
+    let secreto = parseInt(Math.random() * amigos.length);
+    sorteado = amigos[secreto];
+    limparLista();
+    amigos = [];
+    exibirSorteio();
+    botSortear.setAttribute('disabled', true);
+}
+
+function exibirSorteio(){
+    resultado.innerHTML = `O amigo secreto é: ${sorteado}`;
+}
+
+function limparLista(){
+    ulListAmigos.innerHTML = "";
+}
+
+function verifLista(nome){
+    if(amigos.includes(nome.toUpperCase())){
+        alert(`${amigo.value} já existe na lista!`)
+        return true;         
+    }
+}
